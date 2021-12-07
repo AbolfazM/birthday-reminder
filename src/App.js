cc23cfc7b64855c33930508d44b7c8336133cf7f
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-07 13:24:43 +0330

    create components to do!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const [people, setPeople] = useState(data);
+	return(
+		<main>
+			<section className="container">
+				<h3>{people.length} birthdays today</h3>
+				<List people={people} />
+				<button onClick={() => console.log("you clicked me!")}>clear all</button>
+			</section>
+		</main>
+	);
 }
-
 export default App;
