eab918957eb66a0307b88f79c678f0982b177e38
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-07 13:26:40 +0330

    change data!

--- src/data.js
+++ src/data.js
@@ -1,35 +1,35 @@
 export default [
     {
       id: 1,
-      name: 'Bertie Yates',
-      age: 29,
+      name: 'Abolfazl Moradi',
+      age: 18,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
     },
     {
       id: 2,
-      name: 'Hester Hogan',
+      name: 'Amirhossein Moradi',
       age: 32,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
     },
     {
       id: 3,
-      name: 'Larry Little',
+      name: 'Hossein Rahmati',
       age: 36,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
     },
     {
       id: 4,
-      name: 'Sean Walsh',
+      name: 'Sasan Rezaei',
       age: 34,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
     },
     {
       id: 5,
-      name: 'Lola Gardner',
+      name: 'Amir Teymouri',
       age: 29,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
