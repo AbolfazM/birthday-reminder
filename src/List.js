5481b32aee49aecb41d6cafa10796f0ee2042a90
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-07 13:38:36 +0330

    show people!

--- src/List.js
+++ src/List.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
-const List = () => {
+const List = ({people}) => {
   return (
     <>
-      <h2>list component</h2>
+      {
+        people.map((people) => {
+          const {id, name, age, image} = people;
+          return(
+            <article key={id} className="person">
+              <img src={image} alt={name} />
+              <div>
+                <h4>{name}</h4>
+                <p>{age} years old</p>
+              </div>
+            </article>
+          );
+        })
+      }
     </>
   );
 };
 
-export default List;
+export default List; 
