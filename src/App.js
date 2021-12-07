27eb7547a7f060318eceb6b9ac53be3e0f5c95dd
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-07 16:48:16 +0330

    add toastify!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import List from './List';
 import data from './data';
 
 const App = () => {
 	const [people, setPeople] = useState(data);
+	const remove = () => toast.success('people removed!');
+	const restore = () => toast.success('people restored!');
+
+	const removePeople = () => {
+		setPeople([]);
+		remove();
+	}
+	const restorePeople = () => {
+		setPeople(data);
+		restore();
+	}
+
 	return(
 		<main>
 			<section className="container">
 				<h3>{people.length} birthday reminded!</h3>
 				<List people={people} />
-				<button onClick={() => setPeople([])}>clear all!</button>
-				<button onClick={() => setPeople(data)}>restore all!</button>
+				<button onClick={removePeople}>remove all!</button>
+				<button onClick={restorePeople}>restore all!</button>
+				<ToastContainer closeButton={false} autoClose={2000} />
 			</section>
 		</main>
 	);
