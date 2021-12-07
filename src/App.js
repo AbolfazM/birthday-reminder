28fe85879e2a42d63edb50247c49e6044c399aba
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-07 15:38:18 +0330

    update!

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from 'react'
 import List from './List';
+import data from './data';
 
 const App = () => {
 	const [people, setPeople] = useState(data);
 	return(
 		<main>
 			<section className="container">
-				<h3>{people.length} birthdays today</h3>
+				<h3>{people.length} birthday reminded!</h3>
 				<List people={people} />
-				<button onClick={() => setPeople([])}>clear all</button>
+				<button onClick={() => setPeople([])}>clear all!</button>
 			</section>
 		</main>
 	);
 }
+
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,24 +1,20 @@
-import React from 'react';
+import React from 'react'
 
 const List = ({people}) => {
-  return (
-    <>
-      {
-        people.map((people) => {
-          const {id, name, age, image} = people;
-          return(
-            <article key={id} className="person">
-              <img src={image} alt={name} />
-              <div>
-                <h4>{name}</h4>
-                <p>{age} years old</p>
-              </div>
-            </article>
-          );
-        })
-      }
-    </>
-  );
-};
+	return(
+		people.map((people) => {
+			const {id, name, age, image} = people;
+			return(
+				<article key={id} className="person">
+					<img src={image} alt={name} />
+					<div>
+						<h4>{name}</h4>
+						<p>{age} year old</p>
+					</div>
+				</article>
+			);
+		})
+	);
+}
 
-export default List; 
+export default List;
